1b1ac54203ebb19e832a2d33a1f90a5aefc73715
KRISHNA <krishnakantranjan@krishnakants-MacBook-Air.local>
2025-06-23 11:08:57 +0530

    Testing HTTP method

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # devtinder-backend
+
--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+
+app.get('/user', (req, res) => {
+  res.send({firstname: 'John', lastname: 'Doe'});
+}); 
+
+app.post('/user', (req, res) => {
+  // saving data to the database
+  res.send('Data saved successfully!');
+}); 
+
+
+//this will match all routes that are /user.
+app.use('/user', (req, res) => {
   res.send('Hello, World!');
 }); 
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
